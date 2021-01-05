c014b01a09e9e92d076c447f6e7b8790444ec2c9
sepehr <sepehralizade@live.com>
2021-01-05 08:34:10 +0300

    seach component

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,60 @@
+import { IonButton, IonCard, IonInput, IonSelect, IonSelectOption } from '@ionic/react';
+import React, { useState } from 'react';
+import './Search.css';
+
+const Search: React.FC = () => {
+  const [text, setText] = useState<string>('');
+  const [placeholder, setPlaceholder] = useState<string>('Search User');
+  const [searchType, setSearchType] = useState<string>('user');
+  const [searchTime, setSearchTime] = useState<string>('last-week');
+
+  return (
+    <div className="search">
+      <IonCard color={'dark'}>
+        <div className="search__row">
+          <IonInput
+            value={text}
+            placeholder={placeholder}
+            onIonChange={e => setText(e.detail.value!)}
+          ></IonInput>
+          <IonButton
+            className="search__button"
+            color="primary"
+            onClick={e => console.log('clicked')}
+          >
+            Search
+          </IonButton>
+        </div>
+        <div className="search__row">
+          <IonSelect
+            className="search__select"
+            value={searchType}
+            okText="Okay"
+            cancelText="Dismiss"
+            onIonChange={e => {
+              setSearchType(e.detail.value);
+              setPlaceholder(e.detail.value === 'user' ? 'Search User' : 'Search Package');
+            }}
+          >
+            <IonSelectOption value="user">User</IonSelectOption>
+            <IonSelectOption value="package">Package</IonSelectOption>
+          </IonSelect>
+          <IonSelect
+            className="search__select"
+            value={searchTime}
+            okText="Okay"
+            cancelText="Dismiss"
+            onIonChange={e => setSearchTime(e.detail.value)}
+          >
+            <IonSelectOption value="last-day">Last Day</IonSelectOption>
+            <IonSelectOption value="last-week">Last Week</IonSelectOption>
+            <IonSelectOption value="last-month">Last Month</IonSelectOption>
+            <IonSelectOption value="last-year">Last Year</IonSelectOption>
+          </IonSelect>
+        </div>
+      </IonCard>
+    </div>
+  );
+};
+
+export default Search;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,13 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
+import Search from '../components/Search/Search';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <Search />
       </IonContent>
     </IonPage>
   );
